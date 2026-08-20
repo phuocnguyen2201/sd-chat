505cb3d9a961cfbd4d37b4cb09ffa24366ce4d3d
Bruce <phuocnguyen2201@gmail.com>
2026-08-20 23:31:55 +0300

    create random identity code

---
+++ supabase/functions/identityCode/index.ts
@@ -0,0 +1,23 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "npm:@supabase/supabase-js@2";
+
+Deno.serve(async (req: Request) => {
+  try {
+    const randomIdentityCode = (length = 4) => {
+        let result           = '';
+        const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+  } catch (error) {
+    console.error("Unexpected error in function:", error);
+    return new Response(JSON.stringify({ error: "Internal server error", message: String(error) }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+});
